64a7a2a628c825d28c7397fa86306633f8f880a8
Martin Schröder <mkschreder.uk@gmail.com>
2016-10-21 22:52:09 +0200

    just a wip commit..

--- config.h
+++
@@ -1,9 +0,0 @@
-#ifdef BUILD_avr_atmega328p
-#include "include/configs/avr-atmega328p.h"
-#endif
-#ifdef BUILD_arm_stm32f103
-#include "include/configs/arm-stm32f103.h"
-#endif
-#ifdef BUILD_arm_stm32f100mdvl
-#include "include/configs/arm-stm32f100mdvl.h"
-#endif
--- scripts/stm32flash/README.md
+++
@@ -1,1 +0,0 @@
-STM32 flasher utility that flashes STM32 chips thorugh the builtin UART bootloader. You need to reset chip before issuing the flashing command. 
